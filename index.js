0dec9dbaa393fff952ee6132f014ef6d29748353
James Friend <james@jsdf.co>
2015-06-11 22:06:23 +1000

    updated build to use latest babel

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
   Button: require('./lib/Button'),
   Badge: require('./lib/Badge'),
+  ControlItem: require('./lib/ControlItem'),
   Icon: require('./lib/Icon'),
   NavBar: require('./lib/NavBar'),
   NavButton: require('./lib/NavButton'),
+  SegmentedControl: require('./lib/SegmentedControl'),
   TableView: require('./lib/TableView'),
   TableViewCell: require('./lib/TableViewCell'),
   Title: require('./lib/Title'),
