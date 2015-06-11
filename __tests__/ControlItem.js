6709abccd2ee8fee76f3e5d9c94660be88ea56d9
James Friend <james@jsdf.co>
2015-06-11 21:20:37 +1000

    tabs to spaces

--- __tests__/ControlItem.js
+++ __tests__/ControlItem.js
@@ -5,31 +5,31 @@ var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 
 describe('ControlItem', () => {
-	it('should apply the Ratched CSS classes', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<ControlItem />
-		);
-		expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' control-item');
-	});
+  it('should apply the Ratched CSS classes', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <ControlItem />
+    );
+    expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' control-item');
+  });
 
-	it('should apply additional classes', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<ControlItem className='test' />
-		);
-		expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' control-item test');
-	});
+  it('should apply additional classes', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <ControlItem className='test' />
+    );
+    expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' control-item test');
+  });
 
-	it('should set active', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<ControlItem active />
-		);
-		expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' control-item active');
-	});
+  it('should set active', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <ControlItem active />
+    );
+    expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' control-item active');
+  });
 
-	it('should pass children through', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<ControlItem>Test</ControlItem>
-		);
-		expect(React.findDOMNode(ctrl).innerHTML).toBe('Test');
-	});
+  it('should pass children through', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <ControlItem>Test</ControlItem>
+    );
+    expect(React.findDOMNode(ctrl).innerHTML).toBe('Test');
+  });
 });
--- __tests__/SegmentedControl.js
+++ __tests__/SegmentedControl.js
@@ -5,24 +5,24 @@ var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 
 describe('SegmentedControl', () => {
-	it('should set the correct className', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<SegmentedControl />
-		);
-		expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' segmented-control');
-	});
+  it('should set the correct className', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <SegmentedControl />
+    );
+    expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' segmented-control');
+  });
 
-	it('should set addition classes', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<SegmentedControl className='test' />
-		);
-		expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' segmented-control test');
-	});
+  it('should set addition classes', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <SegmentedControl className='test' />
+    );
+    expect(React.findDOMNode(ctrl).getAttribute('class')).toBe(' segmented-control test');
+  });
 
-	it('should pass children props through', () => {
-		var ctrl = TestUtils.renderIntoDocument(
-			<SegmentedControl><span>This is a child</span></SegmentedControl>
-		);
-		expect(React.findDOMNode(ctrl).children.length).toBe(1);
-	});
+  it('should pass children props through', () => {
+    var ctrl = TestUtils.renderIntoDocument(
+      <SegmentedControl><span>This is a child</span></SegmentedControl>
+    );
+    expect(React.findDOMNode(ctrl).children.length).toBe(1);
+  });
 });
